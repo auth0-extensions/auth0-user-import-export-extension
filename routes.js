7b1a5d51cc6acc1e27b3537b0bf5d0cf76d4f5a5
Sandrino Di Mattia <contact@sandrino.be>
2016-04-13 14:16:40 -0300

    v1.3.1

--- routes.js
+++ routes.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Router, Route, IndexRedirect } from 'react-router';
+import { Router, Route, IndexRoute } from 'react-router';
 
 import * as containers from './containers';
 import { RequireAuthentication } from './containers/RequireAuthentication';
@@ -7,7 +7,7 @@ import { RequireAuthentication } from './containers/RequireAuthentication';
 export default (history) =>
   <Router history={history}>
     <Route path="/" component={RequireAuthentication(containers.App)}>
-      <IndexRedirect to="/import" />
+      <IndexRoute component={containers.ImportContainer}/>
       <Route path="export" component={containers.ExportContainer} />
       <Route path="import" component={containers.ImportContainer} />
     </Route>
