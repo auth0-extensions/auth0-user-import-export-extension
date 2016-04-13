9481d0dd0f02bda61c80b7ac527a7dea60b026ff
Sandrino Di Mattia <contact@sandrino.be>
2016-04-13 11:43:48 -0300

    Fix webtask login

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ app.use(auth0({
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use('/meta', (req, res) => {
+app.get('/meta', (req, res) => {
   res.status(200).send(metadata);
 });
 
